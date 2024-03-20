27815bfff66dd06d4fa58c03e617ddd59fb8d1c0
ssus <ssu.private@gmail.com>
2024-03-20 13:01:26 +0900

    filter update

--- js/filter.js
+++ js/filter.js
@@ -1,37 +1,34 @@
-$(window).load(function(){
-    // var $container = $('.news_content');
-    // $container.isotope({
-    //     filter: '*',
-    //     animationOptions: {
-    //         duration: 750,
-    //         easing: 'linear',
-    //         queue: false
-    //     }
-    // });
- 
-    $('.news_filter a').click(function(){
-        $('.news_filter .active').removeClass('active');
-        $(this).addClass('active');
- 
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
-         return false;
-    }); 
+$(document).ready(function () {
+	//filter
+	$(window).load(function () {
+		var $container = $('.news_content');
+		$container.isotope({
+			filter: '*',
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false
+			}
+		});
 
-    var all = $(".news_content li").length;
-    var academy = $(".academy").length;
-    var partner = $(".partner").length;
-    var byfi = $(".byfi").length;
+		$('.news_filter li').click(function () {
+			$('.news_filter .active').removeClass('active');
+			$(this).addClass('active');
 
-    $('#all a').text("ALL ("+all+")");
-    $('#academy a').text("ACADEMY NEWS ("+academy+")");
-    $('#partner a').text("PARTNER NEWS ("+partner+")");
-    $('#byfi a').text("BYFI NEWS ("+byfi+")");
+			var selector = $(this).attr('data-filter');
+			$container.isotope({
+				filter: selector,
+				animationOptions: {
+					duration: 750,
+					easing: 'linear',
+					queue: false
+				}
+			});
+			return false;
+		});
+
+
+
+	});
+
 });
